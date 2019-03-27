e1b0b35c2e1b85406be6994ad63966a35c20af6d
nfrisbee <nicolefrisbee@yahoo.com>
2019-03-26 18:37:45 -0600

    deleted state

--- app1/src/App.js
+++ app1/src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super();
-    
-    this.setstate = {
+    super(props);
+
+    this.state = {
       myName: 'Joe'
     }
   }
   render() {
     return (
       <div className="App">
-        My name is {this.myName}
+        My name is { this.state.myName }
       </div>
     );
   }
