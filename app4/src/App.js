73bd66685702224dddac0b2deeb26747a5a44dd6
nfrisbee <nicolefrisbee@yahoo.com>
2019-03-26 19:16:08 -0600

    added jsx for year, price, model, etc. Mapped over cars. Changes state in the div to just cars jsx

--- app4/src/App.js
+++ app4/src/App.js
@@ -10,20 +10,26 @@ class App extends Component {
     }
   }
 
-  getCars() {
+  getCars =() => {
     axios.get('https://joes-autos.herokuapp.com/api/vehicles')
     .then(res => {
       this.setState({
-        cars: res
+        cars: res.data
       })
     })
   }
 
   render() {
+    const cars = this.state.cars.map(car => (
+      <div>
+       <h3> {car.year} {car.make} {car.model} </h3>
+       <p>{car.price}</p>
+      </div>
+    ));
     return (
       <div className="App">
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+        {cars}
       </div>
     );
   }
